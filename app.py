cbc95b2594de62f740e91dae337a7a846793fcbd
Lukas Rimkus <lukasrimkuslg@gmail.com>
2020-08-02 23:48:51 +0200

    some-styling

--- app.py
+++ app.py
@@ -1,32 +1,46 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, jsonify
+from flask import Flask, flash, request, redirect, url_for, jsonify, render_template
 from werkzeug.utils import secure_filename
 import cv2
 from decouple import config
 from functions.main import getDominantColor, catOrDog, allowed_file
+from jinja2 import Environment, PackageLoader, select_autoescape
+from shutil import copyfile
+
+env = Environment(
+    loader=PackageLoader('app', 'templates'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
 
 
 HOST = config('PORT')
 
 UPLOAD_FOLDER = './Uploads/'
+STATIC_FOLDER = './static/'
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['STATIC_FOLDER'] = STATIC_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 
+title_papge = env.get_template('title.html')
+about_page = env.get_template('about.html')
+
 @app.route('/')
 def hello():
-    return '''
-    	<!doctype html>
-    	<h1>This is Index pages</h1>
-    	<a href='/cats_vs_dogs'>cats vs dogs</a>
-    	'''
+    return render_template(title_papge, title='Index page2')
 
+@app.route('/about')
+def about():
+    return render_template(about_page, title='About page2')
 
 @app.route('/cats_vs_dogs', methods=['GET', 'POST'])
 def upload_file():
+    cats_vs_dogs_page = env.get_template('cats_vs_dogs.html')
+    cats_vs_dogs_results_page = env.get_template('cats_vs_dogs_results.html')
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -39,31 +53,13 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            copyfile(os.path.join(app.config['UPLOAD_FOLDER'], filename), os.path.join(app.config['STATIC_FOLDER'], filename))
             image = cv2.imread(os.path.dirname(os.path.realpath(__file__)) + "/Uploads/" + filename)
             color_result = getDominantColor(image)
             result = catOrDog(image)
             redirect(url_for('upload_file', filename=filename))
+            res = render_template(cats_vs_dogs_results_page, filename = os.path.join(app.config['STATIC_FOLDER'], filename),  title_papge = 'Cats vs Dogs Results', result = result, color_result = color_result)
             os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return '''
-			<!doctype html>
-			<title>Results</title>
-			<h1>Image contains a - ''' + result + '''</h1>
-			<h2>Dominant color is - ''' + color_result + '''</h2>
-			<form method=post enctype=multipart/form-data>
-			  <input type=file name=file>
-			  <input type=submit value=Upload>
-			</form>
-			'''
-    return '''
-	<!doctype html>
-	<title>Upload new File</title>
-	<h1>Upload new File</h1>
-	<form method=post enctype=multipart/form-data>
-	  <input type=file name=file>
-	  <input type=submit value=Upload>
-	</form>
-	'''
-
-
-if __name__ == "__main__":
-    app.run(HOST)
+            os.remove(os.path.join(app.config['STATIC_FOLDER'], filename))
+            return res
+    return render_template(cats_vs_dogs_page, title = 'Upload a new file')
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,15 @@
 from app import app
+from os import path, walk
+
+extra_dirs = ['templates',]
+extra_files = extra_dirs[:]
+for extra_dir in extra_dirs:
+    for dirname, dirs, files in walk(extra_dir):
+        for filename in files:
+            filename = path.join(dirname, filename)
+            if path.isfile(filename):
+                extra_files.append(filename)
+
 
 if __name__ == "__main__":
-	app.run()
+	app.run(extra_files=extra_files)
